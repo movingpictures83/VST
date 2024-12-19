7a75d090910989853390137001c698820799cfb3
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2024-12-19 18:01:06 -0500

    Initial

---
+++ VSTPlugin.R
@@ -0,0 +1,26 @@
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+source("RIO.R")
+
+input <- function(inputfile) {
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+    pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <<- paste(pfix, "/", sep="")
+  }
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+    pbmc <- readRDS(paste(pfix, parameters["rdsfile", 2], sep="/"))
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = as.integer(parameters["features", 2]))
+    saveRDS(pbmc, outputfile)
+}
+
+
